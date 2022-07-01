7d717329a130acb6a049f8b9f364a06be138cb71
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-02 00:29:04 +0530

    feat: Events page

---
+++ app/routes/Events/index.js
@@ -0,0 +1,19 @@
+import MockNavigator from "~/Components/MockNavigator/MockNavigator";
+import styleSheet from "~/styles/routes/Events/Events.css";
+
+export function links() {
+  return [{ rel: "stylesheet", href: styleSheet }];
+}
+
+export default function Index() {
+  return (
+    <div className="EventsPage">
+      <MockNavigator title="Events">
+        <a href="/">Home</a>
+        <a href="/admin">Admin</a>
+        <a href="/navigation">Navigation</a>
+        <a href="/faqs">FAQs</a>
+      </MockNavigator>
+    </div>
+  );
+}
