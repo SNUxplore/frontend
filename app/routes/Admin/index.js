a60d6506534804f0ec131a5f6633c904c3e3cb9e
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-02 00:28:49 +0530

    feat: Admin page

---
+++ app/routes/Admin/index.js
@@ -0,0 +1,19 @@
+import MockNavigator from "~/Components/MockNavigator/MockNavigator";
+import styleSheet from "~/styles/routes/Admin/Admin.css";
+
+export function links() {
+  return [{ rel: "stylesheet", href: styleSheet }];
+}
+
+export default function Index() {
+  return (
+    <div className="AdminPage">
+      <MockNavigator title="Admin">
+        <a href="/">Home</a>
+        <a href="/navigation">Navigation</a>
+        <a href="/events">Events</a>
+        <a href="/faqs">FAQs</a>
+      </MockNavigator>
+    </div>
+  );
+}
