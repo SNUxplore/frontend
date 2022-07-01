df658b3c191a3ebcc032668d9106edb1bf976685
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-02 00:26:30 +0530

    feat(Root): Ignore .css files in dev mode

--- app/root.jsx
+++ app/root.jsx
@@ -7,19 +7,21 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-import resetStyle from "./styles/reset.css";
-import globalStyle from "./styles/global.css";
+import resetStyle from "./styles/root/reset.css";
+import globalStyle from "./styles/root/global.css";
+import componentStyle from "./styles/root/components.css";
 
 export function links() {
   return [
     { rel: "stylesheet", href: resetStyle },
     { rel: "stylesheet", href: globalStyle },
+    { rel: "stylesheet", href: componentStyle },
   ];
 }
 
 export const meta = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "SnuXplore",
   viewport: "width=device-width,initial-scale=1",
 });
 
