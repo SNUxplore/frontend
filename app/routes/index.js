3a796766cf6000a44a38c6e024ce8c37230628e5
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-04 23:48:11 +0530

    chore: Reset the landing page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,3 @@
-import MockNavigator from "~/Components/MockNavigator/MockNavigator";
 import styleSheet from "~/styles/routes/LandingPage.css";
 
 export function links() {
@@ -6,14 +5,5 @@ export function links() {
 }
 
 export default function LandingPage() {
-  return (
-    <div className="LandingPage">
-      <MockNavigator title="Landing Page">
-        <a href="/navigation">Navigation</a>
-        <a href="/admin">Admin</a>
-        <a href="/events">Events</a>
-        <a href="/faqs">FAQs</a>
-      </MockNavigator>
-    </div>
-  );
+  return <div className="LandingPage"></div>;
 }
