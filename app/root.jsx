324f7850e7a73a2cdfaf71c3ec1035443617b6d6
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-05 13:01:30 +0530

    chore: FavIcon added

--- app/root.jsx
+++ app/root.jsx
@@ -11,13 +11,21 @@ import resetStyle from "./styles/root/reset.css";
 import globalStyle from "./styles/root/global.css";
 import componentStyle from "./styles/root/components.css";
 
+import favIcon from "app/Assets/Img/xploreLogo.svg";
+
 export function links() {
   return [
     { rel: "stylesheet", href: resetStyle },
     { rel: "stylesheet", href: globalStyle },
     { rel: "stylesheet", href: componentStyle },
+    {
+      rel: "icon",
+      href: favIcon,
+      type: "image/png",
+    },
   ];
 }
+
 
 export const meta = () => ({
   charset: "utf-8",
