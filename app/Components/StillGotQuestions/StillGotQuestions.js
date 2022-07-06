0372a994349b61b9c825dba2f4591b947ff3466c
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-06 23:37:11 +0530

    chore: html structure modified

--- app/Components/StillGotQuestions/StillGotQuestions.js
+++ app/Components/StillGotQuestions/StillGotQuestions.js
@@ -3,21 +3,19 @@ import ButtonLink from "~/Components/ButtonLink/ButtonLink";
 
 function StillGotQuestions() {
   return (
-    <div className="QuesBackdrop">
-      <div className="SGQcontainer">
-        <div className="SGQleft BigText">
-          <p className="SGQtext">
-            <span className="StillHave">Still have</span>{" "}
-            <span className="spanQuestions">Questions?</span>
-          </p>
-        </div>
-        <div className="SGQright">
-          <p className="SGQtext SmallText">
+    <div className="FaqRedirect">
+      <div className="FaqRedirect__left">
+        <h1 className="FaqRedirect__title--white">Still have</h1>
+        <h1 className="FaqRedirect__title--black">Questions?</h1>
+      </div>
+      <div className="FaqRedirect__right">
+        <div className="FaqRedirect__right--container">
+          <p className="FaqRedirect--desc">
             Dignissim morbi scelerisque a mauris nec tellus orci, fringilla. Leo
             lorem arcu donec lectus netus eros. Massa sit magna lacus lorem eu
             metus sem.
           </p>
-          <ButtonLink href="/#faq" content="Checkout the FAQ" fill2 />
+          <ButtonLink href="/faq" content="Checkout the FAQ" fill2 />
         </div>
       </div>
     </div>
