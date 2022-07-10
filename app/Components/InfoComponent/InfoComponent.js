09907ca3fa94fc44c7fc6af22a905e59538dc187
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-07-10 19:56:45 +0530

    feat: added indicators jsx + functionality

--- app/Components/InfoComponent/InfoComponent.js
+++ app/Components/InfoComponent/InfoComponent.js
@@ -1,7 +1,35 @@
 import React from "react";
 
-function InfoComponent({ title, information, button }) {
-  return <div className="InfoComponent__Container">Hi</div>;
+function InfoComponent({ title, information, button, indicatorState = 1 }) {
+  return (
+    <div className="InfoComponent__Container">
+      <div className="InfoComponent__content--container">
+        <div className="InfoComponent__content--container__indicator--container">
+          <div
+            className={
+              indicatorState === 1
+                ? "InfoComponent__content--container__indicator--container__indicator active"
+                : "InfoComponent__content--container__indicator--container__indicator"
+            }
+          />
+          <div
+            className={
+              indicatorState === 2
+                ? "InfoComponent__content--container__indicator--container__indicator active"
+                : "InfoComponent__content--container__indicator--container__indicator"
+            }
+          />
+          <div
+            className={
+              indicatorState === 3
+                ? "InfoComponent__content--container__indicator--container__indicator active"
+                : "InfoComponent__content--container__indicator--container__indicator"
+            }
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default InfoComponent;
