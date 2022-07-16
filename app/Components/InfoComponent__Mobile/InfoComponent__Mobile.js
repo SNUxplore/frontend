1f6aaca2aa37bf4a438e70b6a77de5d384c9ec81
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-07-17 01:21:21 +0530

    feat: added mobile comp

--- app/Components/InfoComponent__Mobile/InfoComponent__Mobile.js
+++ app/Components/InfoComponent__Mobile/InfoComponent__Mobile.js
@@ -1,3 +1,5 @@
+import Feature3Img from "../../Assets/Img/Feature-3.svg";
+import Feature2Img from "../../Assets/Img/Feature-2.svg";
 import Feature1Img from "../../Assets/Img/Feature-1.svg";
 import arrowLeft from "../../Assets/Img/arrowLeft.svg";
 import arrowRight from "../../Assets/Img/arrowRight.svg";
@@ -19,16 +21,40 @@ export default function InfoComponent__Mobile() {
   return (
     <div className="MobileContainer">
       <h1 className="MobileContainer__title">
-        Navigate <span>Campus</span>
+        {infoIndex === 0
+          ? "Navigate"
+          : infoIndex === 1
+          ? "Happening"
+          : "Admin &"}{" "}
+        <span>
+          {infoIndex === 0 ? "Campus" : infoIndex === 1 ? "@SNU" : "Info"}
+        </span>
       </h1>
       <p className="MobileContainer__desc">
         We get it, navigating the university can be challenging! But have no
         worries, We can connect you to resourses that will unlock all that Shiv
         Nadar University has to offer.
       </p>
-      <ButtonLink content="Explore now" />
+      <ButtonLink
+        content={
+          infoIndex === 0
+            ? "Explore now"
+            : infoIndex === 1
+            ? "Find an Event"
+            : "Admin & Info"
+        }
+      />
       <div className="MobileContainer__ImageContainer">
-        <img src={Feature1Img}></img>
+        <img
+          className="MobileContainer__ImageContainer__image"
+          src={
+            infoIndex === 0
+              ? Feature1Img
+              : infoIndex === 1
+              ? Feature2Img
+              : Feature3Img
+          }
+        ></img>
       </div>
       <div className="MobileContainer__scrollerContainer">
         <img
