301b881a305e91e46c728e1b1cf455400bf8def4
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-07-17 00:37:50 +0530

    feat: added functional indicators with animation

--- app/Components/InfoComponent__Mobile/InfoComponent__Mobile.js
+++ app/Components/InfoComponent__Mobile/InfoComponent__Mobile.js
@@ -1,8 +1,20 @@
 import Feature1Img from "../../Assets/Img/Feature-1.svg";
 import arrowLeft from "../../Assets/Img/arrowLeft.svg";
 import arrowRight from "../../Assets/Img/arrowRight.svg";
+import React from "react";
 
 export default function InfoComponent__Mobile() {
+  const [infoIndex, setInfoIndex] = React.useState(0);
+
+  const indicatorScrollRight = () => {
+    infoIndex < 2 ? setInfoIndex(infoIndex + 1) : 2;
+    console.log(infoIndex);
+  };
+
+  const indicatorScrollLeft = () => {
+    infoIndex > 0 ? setInfoIndex(infoIndex - 1) : 0;
+  };
+
   return (
     <div className="MobileContainer">
       <h1 className="MobileContainer__title">
@@ -17,9 +29,21 @@ export default function InfoComponent__Mobile() {
         <img src={Feature1Img}></img>
       </div>
       <div className="MobileContainer__scrollerContainer">
-        <img src={arrowLeft} />
-        <div className="MobileContainer__scrollerContainer__indicators"></div>
-        <img src={arrowRight} />
+        <img
+          className="MobileContainer__scrollerContainer__arrow"
+          src={arrowLeft}
+          onClick={() => indicatorScrollLeft()}
+        />
+        <div className="MobileContainer__scrollerContainer__indicators">
+          <div className={infoIndex === 0 ? "circle active" : "circle"}></div>
+          <div className={infoIndex === 1 ? "circle active" : "circle"}></div>
+          <div className={infoIndex === 2 ? "circle active" : "circle"}></div>
+        </div>
+        <img
+          className="MobileContainer__scrollerContainer__arrow"
+          src={arrowRight}
+          onClick={() => indicatorScrollRight()}
+        />
       </div>
     </div>
   );
