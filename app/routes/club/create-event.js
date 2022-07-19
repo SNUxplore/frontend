3ac2e23e0095e30f7f43e74caa601b7f228a3512
Pratham Aggarwal <prathu.agg@gmail.com>
2022-07-19 17:34:40 +0530

    fix(create-event) working now

--- app/routes/club/create-event.js
+++ app/routes/club/create-event.js
@@ -14,9 +14,12 @@ export const action = async ({ request }) => {
   const formData = Object.fromEntries(await request.formData());
   // all your form data is here
   console.log(formData);
+  formData.userId = parseInt(formData.userId);
+
   const data = createEvent(formData)
     .then((res) => res)
     .catch((e) => {
+      console.log(e);
       throw new Error("Create event error");
     });
   return data;
@@ -34,13 +37,30 @@ export default function CreateEvent() {
         {/* Cap title to max 200 character, i.e. make this a controlled component */}
         <label htmlFor="title">title</label>
         <input placeholder="title" id="title" name="title" />
+
         <label htmlFor="location">location</label>
         <input placeholder="location" id="location" name="location" />
+
+        <label htmlFor="imageUrl">image</label>
+        <input placeholder="imageUrl" id="imageUrl" name="imageUrl" />
+
+        <label htmlFor="date">Date</label>
+        <input placeholder="date" id="date" name="date" />
+
         <label htmlFor="description">Description</label>
         <textarea
           placeholder="description"
           id="description"
           name="description"
+        />
+
+        {/* Do not edit this, its to feed userId into the form*/}
+        <input
+          id="userId"
+          name="userId"
+          style={{ visibility: "hidden", display: "none" }}
+          type="number"
+          defaultValue={data.id}
         />
         <button type="submit">Submit</button>
       </Form>
