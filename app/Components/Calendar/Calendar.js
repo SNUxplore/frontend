d1764a6d90ec08179050bcfe0cf1384fe98354cc
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-07-22 04:12:47 +0530

    refactor: added component renders to useEffect

--- app/Components/Calendar/Calendar.js
+++ app/Components/Calendar/Calendar.js
@@ -2,22 +2,26 @@ import React from "react";
 import moment from "moment";
 
 const Calendar = () => {
-  const [monthData, setMonthData] = React.useState([]);
+  const [calendarData, setCalendarData] = React.useState([]); //contains weeks , weeks contain days
   const [value, setValue] = React.useState(moment()); //gets the local machine date
 
   const firstDay = value.clone().startOf("month").startOf("week"); //first day on calendar grid
   const lastDay = value.clone().endOf("month").endOf("week"); //last day on calendar grid
-  const day = firstDay.clone().subtract(1, "day"); //starts at a prev index for looping purposes [iterator]
-  const calendar = []; //contains weeks , weeks contain days
 
-  while (day.isBefore(lastDay, "day")) {
-    calendar.push(
-      //array contains weeks
-      Array(7) //array for week
-        .fill(0)
-        .map(() => day.add(1, "day").clone()) //days cloned and iterated for weeks
-    );
-  }
+  React.useEffect(() => {
+    const effectArray = [];
+    const day = firstDay.clone().subtract(1, "day"); //starts at a prev index for looping purposes [iterator]
+    while (day.isBefore(lastDay, "day")) {
+      effectArray.push(
+        //array contains weeks
+        Array(7) //array for week
+          .fill(0)
+          .map(() => day.add(1, "day").clone()) //days cloned and iterated for weeks
+      );
+    }
+    setCalendarData(effectArray);
+  }, [value]);
+
   return (
     <div className="calendar-container">
       <div className="calendar-container__daynames">
@@ -29,7 +33,7 @@ const Calendar = () => {
         <p className="calendar-container__daynames__day">Fri</p>
         <p className="calendar-container__daynames__day">Sat</p>
       </div>
-      {calendar.map((week) => (
+      {calendarData.map((week) => (
         <div className="calendar-container__week-container">
           {week.map((day) => (
             <div className="calendar-container__day-container">
