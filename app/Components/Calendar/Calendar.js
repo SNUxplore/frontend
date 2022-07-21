041670ef1686f843f665e0c6fc0bfcdb1903a30b
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-07-22 04:01:34 +0530

    chore: added states

--- app/Components/Calendar/Calendar.js
+++ app/Components/Calendar/Calendar.js
@@ -2,7 +2,9 @@ import React from "react";
 import moment from "moment";
 
 const Calendar = () => {
-  const value = moment(); //gets the local machine date
+  const [monthData, setMonthData] = React.useState([]);
+  const [value, setValue] = React.useState(moment()); //gets the local machine date
+
   const firstDay = value.clone().startOf("month").startOf("week"); //first day on calendar grid
   const lastDay = value.clone().endOf("month").endOf("week"); //last day on calendar grid
   const day = firstDay.clone().subtract(1, "day"); //starts at a prev index for looping purposes [iterator]
