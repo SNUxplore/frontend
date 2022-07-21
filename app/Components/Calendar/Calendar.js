4f4887a0d746f16d872adf5b77d0df9477875349
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-07-22 02:49:58 +0530

    feat: generated first and last days

--- app/Components/Calendar/Calendar.js
+++ app/Components/Calendar/Calendar.js
@@ -2,7 +2,11 @@ import React from "react";
 import moment from "moment";
 
 const Calendar = () => {
-  return <div>hi</div>;
+  const value = moment(); //gets the local machine date
+  const firstDay = value.clone().startOf("month").startOf("week"); //first day on calendar grid
+  const lastDay = value.clone().endOf("month").endOf("week"); //last day on calendar grid
+  const day = firstDay.clone().subtract(1, "day"); //starts at a prev index for looping purposes
+  return <div>{firstDay.format("DD/MM/YY")}</div>;
 };
 
 export default Calendar;
