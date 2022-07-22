14f81348d633dc0e5b0ed581ad22a60c4a76cffc
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-22 23:08:15 +0530

    refactor: Place card

--- app/Components/PlaceCard/PlaceCard.js
+++ app/Components/PlaceCard/PlaceCard.js
@@ -8,117 +8,73 @@ import foodMenuLogo from "../../Assets/Img/foodMenu.svg";
 
 import { useMediaQuery } from "react-responsive";
 
-function PlaceCard({
-	name,
-	actionLists,
-	desc,
-	src,
-	highlighted,
-}) {
+function PlaceCard({ name, actionLists, desc, src, highlighted }) {
   const iconShow = useMediaQuery({
     query: "(max-width: 450px)",
   });
   const [isOpen, setIsOpen] = React.useState(false);
   const toggling = () => setIsOpen(!isOpen);
-  const names = "content";
 
-  // var styles;
-
-  // if (iconShow) {
-  //   if (isOpen) {
-  //     styles = {
-  //       height: "fit-content",
-  //     };
-  //   } else {
-  //     styles = {
-  //       overflow: "hidden",
-  //       height: "0",
-  //     };
-  //   }
-  // } else {
-  //   styles = { width: "100%", height: "auto" };
-  // }
   return (
-		<div
-			id={name}
-			className={`
-			PlaceCardWrapper
-			${highlighted ? "PlaceCardWrapper--highlighted" : ""}
-			`}>
-      <div className="PlaceCardWrapper__content">
-        <img
-          className="PlaceCardWrapper--image"
-          src={src != "" ? src : placeholder}
-          alt="Placeholder"
-        />
-        <div className="PlaceCardWrapper__conAndActions ">
-          <div className="PlaceCardWrapper__content--text">
-            <h2 className="PlaceCardWrapper__content--title" onClick={toggling}>
-              {name}
-            </h2>
+    <div
+      id={name}
+      className={`PlaceCardWrapper ${
+        highlighted ? "PlaceCardWrapper--highlighted" : ""
+      }`}
+    >
+      <div className="PlaceCardWrapper__content ">
+      <img
+        className="PlaceCardWrapper--image"
+        src={src != "" ? src : placeholder}
+        alt="Placeholder"
+      />
+        <div className="PlaceCardWrapper__content--text">
+          <h2 className="PlaceCardWrapper__content--title" onClick={toggling}>
+            {name}
+          </h2>
 
-            <div id={names}>
-              {" "}
-              {/* style={styles}*/}
-              <p className="PlaceCardWrapper__content--desc">{desc}</p>
-              {/* <p className="PlaceCardWrapper__content--poc">
-          <span>Receptionist: </span>Gravida Mouna
-        </p> */}
-              <div className="PlaceCardWrapper__actions">
-                {actionLists.map((action, index) => (
-                  <a
-                    key={index}
-                    className="PlaceCardWrapper__actions--link"
-                    href={action.href}
-                    target="_blank"
-                  >
-                    {(() => {
-                      if (action.title == "Map") {
-                        return (
-                          <img
-                            className="PlaceCardWrapper__actions--icon"
-                            src={mapLogo}
-                            alt="map"
-                          />
-                        );
-                      }
-
-                      if (iconShow && action.title == "Call") {
-                        return (
-                          <img
-                            className="PlaceCardWrapper__actions--icon"
-                            src={callLogo}
-                            alt="call"
-                          />
-                        );
-                      }
-
-                      if (iconShow && action.title == "Mail") {
-                        return (
-                          <img
-                            className="PlaceCardWrapper__actions--icon"
-                            src={mailLogo}
-                            alt="mail"
-                          />
-                        );
-                      }
-                      if (iconShow && action.title == "Menu") {
-                        return (
-                          <img
-                            className="PlaceCardWrapper__actions--icon"
-                            src={foodMenuLogo}
-                            alt="mail"
-                          />
-                        );
-                      }
-                    })()}
-                    {action.title}
-                  </a>
-                ))}
-              </div>
-            </div>
-          </div>
+          <p className="PlaceCardWrapper__content--desc">{desc}</p>
         </div>
+      </div>
+      <div className="PlaceCardWrapper__actions">
+        {actionLists.map((action, index) => (
+          <a
+            key={index}
+            className="PlaceCardWrapper__actions--link"
+            href={action.href}
+            target="_blank"
+          >
+            {action.title == "Map" && (
+              <img
+                className="PlaceCardWrapper__actions--icon"
+                src={mapLogo}
+                alt="map"
+              />
+            )}
+            {iconShow && action.title == "Call" && (
+              <img
+                className="PlaceCardWrapper__actions--icon"
+                src={callLogo}
+                alt="call"
+              />
+            )}
+            {iconShow && action.title == "Mail" && (
+              <img
+                className="PlaceCardWrapper__actions--icon"
+                src={mailLogo}
+                alt="mail"
+              />
+            )}
+            {iconShow && action.title == "Menu" && (
+              <img
+                className="PlaceCardWrapper__actions--icon"
+                src={foodMenuLogo}
+                alt="mail"
+              />
+            )}
+            {action.title}
+          </a>
+        ))}
       </div>
     </div>
   );
