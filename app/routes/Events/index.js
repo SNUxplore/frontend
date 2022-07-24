d3dd39c5e4a15c7c93a0d2514f3559a91db6bb16
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-24 14:37:07 +0530

    feat: working events page

--- app/routes/Events/index.js
+++ app/routes/Events/index.js
@@ -1,5 +1,9 @@
 import { useLoaderData } from "@remix-run/react";
-import MockNavigator from "~/Components/MockNavigator/MockNavigator";
+import Banner from "~/Components/Banner/Banner";
+import Calendar from "~/Components/Calendar/Calendar";
+import EventCard from "~/Components/EventCard/EventCard";
+import Footer from "~/Components/Footer/Footer";
+import Header from "~/Components/Header/Header";
 import styleSheet from "~/styles/routes/Events/Events.css";
 import { getEvents } from "../services/club.server";
 
@@ -16,17 +20,82 @@ export const loader = async ({ request }) => {
 };
 
 export default function Index() {
-  const data = useLoaderData();
-  // receives data, use it wisely
-  console.log(data);
+  const originalData = useLoaderData();
+  const [data, setData] = React.useState(useLoaderData());
+  const [selectedDate, setSelectedDate] = React.useState(null);
+
+  React.useEffect(() => {
+    if (
+      selectedDate !== null &&
+      data.filter((event) => event.date === selectedDate).length != 0
+    ) {
+      setData(data.filter((event) => event.date === selectedDate));
+    } else {
+      setData(originalData);
+      setSelectedDate(null);
+    }
+  }, [selectedDate]);
+
+  React.useEffect(() => {
+    window.addEventListener("load", () => {
+      const height = document.querySelector(".EventsPage__right").clientHeight;
+      console.log(height);
+      document.querySelector(".EventsPage__left").style.height = `${height}px`;
+    });
+    window.addEventListener("resize", () => {
+      const height = document.querySelector(".EventsPage__right").clientHeight;
+      document.querySelector(".EventsPage__left").style.height = `${height}px`;
+    });
+  }, []);
   return (
     <div className="EventsPage">
-      <MockNavigator title="Events">
-        <a href="/">Home</a>
-        <a href="/admin">Admin</a>
-        <a href="/navigate">Navigation</a>
-        <a href="/faqs">FAQs</a>
-      </MockNavigator>
+      <Header />
+      <main className="EventsPage__mainContainer">
+        {/* <div className="EventsPage__top">
+        </div> */}
+        <div className="EventsPage__left">
+          <h2 className="EventsPage__left--title">
+            Happening <span>@SNU</span>
+          </h2>
+          <p className="EventsPage__left--subTitle">
+            Don’t miss out on any scheduled events
+          </p>
+          <div className="EventsPage__left--events">
+            {data.map((event, index) => (
+              <EventCard
+                key={index}
+                eventName={event.title}
+                location={event.location}
+                date={event.date}
+                clubName="ECell"
+                desc={event.description}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="EventsPage__right">
+          <div className="EventsPage__right--calender">
+            <Calendar
+              events={data.map((item) => ({
+                eventName: item.title,
+                eventDate: item.date,
+              }))}
+              setSelectedDate={setSelectedDate}
+            />
+          </div>
+        </div>
+      </main>
+      <Banner
+        title1="Wish to add"
+        title2="club events?"
+        desc="Dignissim morbi scelerisque a mauris nec tellus orci, fringilla. Leo
+            lorem arcu donec lectus netus eros. Massa sit magna lacus lorem eu
+            metus sem."
+        href="/faqs"
+        fillType="fill2"
+        redirectContent="Club Login"
+      />
+      <Footer />
     </div>
   );
 }
