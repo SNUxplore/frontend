bf035e2aab1a757ca8b7325c2c894171153cda05
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-24 14:36:48 +0530

    chore: redirect path of logged in members

--- app/routes/login.js
+++ app/routes/login.js
@@ -3,7 +3,7 @@ import { authenticator } from "./services/auth.server";
 
 export const loader = async ({ request }) => {
   const user = await authenticator.isAuthenticated(request, {
-    successRedirect: "/create-event",
+    successRedirect: "/club/create-event",
   });
   console.log(user);
   return null;
