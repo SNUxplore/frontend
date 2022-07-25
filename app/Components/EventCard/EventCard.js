a4b6b68196f3757af7ba93ff194ef9194aa97cfa
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-07-25 23:17:13 +0530

    feat: rendered relavent details on modal

--- app/Components/EventCard/EventCard.js
+++ app/Components/EventCard/EventCard.js
@@ -1,12 +1,14 @@
+import React from "react";
 import logoPreview from "../../Assets/Img/twitterLogo.svg";
 import InfoModal from "../infoModal/InfoModal";
 
 function EventCard({ eventName, logo, clubName, date, location, more, desc }) {
   const [isModalOpen, setModalOpen] = React.useState(false);
+  const [eventData, setEventData] = React.useState([]);
   return (
     <div className="EventCardWrapper">
       {isModalOpen && (
-        <InfoModal data={eventName} setModalOpen={setModalOpen} />
+        <InfoModal data={eventData} setModalOpen={setModalOpen} />
       )}
       <div className="EventCardWrapper__left">
         <div className="EventCardWrapper__left--top">
@@ -20,7 +22,18 @@ function EventCard({ eventName, logo, clubName, date, location, more, desc }) {
         <p>{date}</p>
         <p>{location}</p>
         <button
-          onClick={() => setModalOpen(true)}
+          onClick={() => {
+            setEventData([
+              eventName,
+              logo,
+              clubName,
+              date,
+              location,
+              more,
+              desc,
+            ]);
+            setModalOpen(true);
+          }}
           className="EventCardWrapper__right--button"
         >
           More Details
