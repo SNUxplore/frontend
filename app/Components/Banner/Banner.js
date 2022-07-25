edeb4483780deba02a3bc2161ecce7ae2fcae2a1
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-25 21:52:51 +0530

    chore: full width banner

--- app/Components/Banner/Banner.js
+++ app/Components/Banner/Banner.js
@@ -1,23 +1,25 @@
 import React from "react";
 import ButtonLink from "~/Components/ButtonLink/ButtonLink";
 
-function Banner({ title1, title2, desc, href, fillType,redirectContent }) {
+function Banner({ title1, title2, desc, href, fillType, redirectContent }) {
   return (
     <div className="Banner">
-      <div className="Banner__left">
-        <h1 className="Banner__title--white">{title1}</h1>
-        <h1 className="Banner__title--primaryTextColor">{title2}</h1>
-      </div>
-      <div className="Banner__right">
-        <div className="Banner__right--container">
-          <p className="Banner--desc">{desc}</p>
-          <ButtonLink
-            className="Banner--button"
-            href={href}
-            content={redirectContent}
-            fill={fillType === "fill"}
-            fill2={fillType === "fill2"}
-          />
+      <div className="Banner__Container">
+        <div className="Banner__left">
+          <h1 className="Banner__title--white">{title1}</h1>
+          <h1 className="Banner__title--primaryTextColor">{title2}</h1>
+        </div>
+        <div className="Banner__right">
+          <div className="Banner__right--container">
+            <p className="Banner--desc">{desc}</p>
+            <ButtonLink
+              className="Banner--button"
+              href={href}
+              content={redirectContent}
+              fill={fillType === "fill"}
+              fill2={fillType === "fill2"}
+            />
+          </div>
         </div>
       </div>
     </div>
