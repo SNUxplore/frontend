e49bca2b841855905e84b6be4b00074e2d7a320f
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-07-26 16:01:07 +0530

    feat: modal styled

--- app/Components/infoModal/InfoModal.js
+++ app/Components/infoModal/InfoModal.js
@@ -5,6 +5,11 @@ import dateIcon from "../../Assets/Img/modaldateIcon_lightTheme.svg";
 import timeIcon from "../../Assets/Img/modalTimeIcon_lightTheme.svg";
 import locationIcon from "../../Assets/Img/modalLocationIcon_lightTheme.svg";
 
+import clubInstagram from "../../Assets/Img/insta_dark.svg";
+import clubTwitter from "../../Assets/Img/twitter_dark.svg";
+import clubFacebook from "../../Assets/Img/facebook_dark.svg";
+import modalCloseIcon from "../../Assets/Img/modalClose__LightTheme.svg";
+
 function InfoModal({ setModalOpen, data }) {
   return (
     <div className="ModalWrapper">
@@ -14,7 +19,7 @@ function InfoModal({ setModalOpen, data }) {
           className="ModalWrapper__container--closebtn"
           onClick={() => setModalOpen(false)}
         >
-          close
+          <img src={modalCloseIcon} alt="modalCloseIcon" />
         </button>
         <div className="ModalWrapper__container__infoContainer">
           <h1 className="ModalWrapper__container__infoContainer--title">
@@ -22,41 +27,69 @@ function InfoModal({ setModalOpen, data }) {
             {data[2]}
           </h1>
           <p className="ModalWrapper__container__infoContainer--details">
-            <img src="" alt="fire icon" />
+            <img
+              className="ModalWrapper__container__infoContainer--icon"
+              src={fireIcon}
+              alt="fire icon"
+            />
             <span>Event :</span> {data[0]}
           </p>
           <p className="ModalWrapper__container__infoContainer--details">
-            <img src="" alt="time icon" />
+            <img
+              className="ModalWrapper__container__infoContainer--icon"
+              src={timeIcon}
+              alt="time icon"
+            />
             <span>Timings :</span> --
           </p>
           <p className="ModalWrapper__container__infoContainer--details">
-            <span>
-              {" "}
-              <img src="" alt="date icon" />
-              Date :
-            </span>
+            <img
+              className="ModalWrapper__container__infoContainer--icon"
+              src={dateIcon}
+              alt="date icon"
+            />
+            <span>Date :</span>
             {data[3]}
           </p>
           <p className="ModalWrapper__container__infoContainer--details">
-            <span>
-              {" "}
-              <img src="" alt="Location icon" />
-              Location :
-            </span>
-            {data[4]}
+            <img
+              className="ModalWrapper__container__infoContainer--icon"
+              src={locationIcon}
+              alt="Location icon"
+            />
+            <span>Location :</span> {data[4]}
           </p>
           <h3 className="ModalWrapper__container__infoContainer--titleSecondary">
             Description
           </h3>
-          <p className="ModalWrapper__container__inforContainer--eventDesc">
+          <p className="ModalWrapper__container__infoContainer--eventDesc">
             {data[6]}
           </p>
           <p className="ModalWrapper__container__infoContainer--titleSecondary">
-            social icons here
+            Club socials
           </p>
-          <a href="www.google.com">
-            <img src="" alt="link icon" />
-            www.cowsAndBears.com
+          <div className="ModalWrapper__container__infoContainer--socialsWrapper">
+            <img
+              src={clubInstagram}
+              alt="clubInsta"
+              className="ModalWrapper__container__infoContainer--Socialsicon"
+            />
+            <img
+              src={clubTwitter}
+              alt="clubTwitter"
+              className="ModalWrapper__container__infoContainer--SocialsiconTW"
+            />
+            <img
+              className="ModalWrapper__container__infoContainer--Socialsicon"
+              src={clubFacebook}
+              alt="clubFacebook"
+            />
+          </div>
+          <a
+            className="ModalWrapper__container__infoContainer--linkHighlight"
+            href="https://ecellsnu.com/home"
+          >
+            https://ecellsnu.com/home
           </a>
         </div>
       </div>
