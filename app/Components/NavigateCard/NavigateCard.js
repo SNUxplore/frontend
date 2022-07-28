3b33237445580f2868cb30a7394811071864e624
riyasr21 <rs327@snu.edu.in>
2022-07-29 01:45:38 +0530

    feat: Map markers added

--- app/Components/NavigateCard/NavigateCard.js
+++ app/Components/NavigateCard/NavigateCard.js
@@ -1,13 +1,6 @@
 import { PhoneCall, EnvelopeSimpleOpen } from "phosphor-react";
 import placeholder from "~/Assets/Img/PlaceCard.png";
 
-function getLongLat(url) {
-  var regex = new RegExp("@(.*),(.*),");
-  const latLongMatch = url.match(regex);
-  console.log(latLongMatch);
-  return latLongMatch;
-}
-
 export default function NavigateCard({
   name,
   actionLists,
@@ -27,7 +20,6 @@ export default function NavigateCard({
   const menu = actionLists.filter((action) => {
     return action.title == "Menu";
   });
-  let lo;
 
   return (
     <div
@@ -44,12 +36,7 @@ export default function NavigateCard({
         <p className="NavCardContainer__content__desc">{desc}</p>
         <div className="NavCardContainer__content__ctas">
           <a href={location[0].href}>
-            <button
-              className="location-button"
-              onClick={(lo = getLongLat(location[0].href))}
-            >
-              Location
-            </button>
+            <button className="location-button">Location</button>
           </a>
           {menu.length == 0 ? (
             ""
