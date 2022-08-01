31799198c2a226f6b0214df1b0946a324a522fb1
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-08-01 17:23:43 +0530

    chore: fixed react call

--- app/routes/Events/index.js
+++ app/routes/Events/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useLoaderData } from "@remix-run/react";
 import Banner from "~/Components/Banner/Banner";
 import Calendar from "~/Components/Calendar/Calendar";
