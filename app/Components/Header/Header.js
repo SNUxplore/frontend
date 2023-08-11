218279354188152d38e0b591096518ed3a1192e0
Prabhav <prabhavp380@gmail.com>
2023-08-11 23:31:16 +0530

    chore: removed comments

--- app/Components/Header/Header.js
+++ app/Components/Header/Header.js
@@ -1,21 +1,7 @@
 import React from "react";
 // import appLogo from "../../Assets/Img/newLogo.svg";
 import appLogo from "../../Assets/Img/logo.svg";
-import instagramLogo from "../../Assets/Img/instagramLogo.svg";
-import linkedInLogo from "../../Assets/Img/linkedInLogo.svg";
-import locationIcon from "../../Assets/Img/locationIcon.svg";
-import fireIcon from "../../Assets/Img/fireIcon.svg";
-import infoIcon from "../../Assets/Img/infoIcon.svg";
-import questionIcon from "../../Assets/Img/questionIcon.svg";
-import usersIcon from "../../Assets/Img/usersIcon.svg";
-import lightModeDesktop from "../../Assets/Img/lightModeDesktop.svg";
-import lightModeIcon from "../../Assets/Img/lightModeIcon.svg";
 import darkModeDesktop from "../../Assets/Img/darkModeThemeDesktop.svg";
-import darkModeIcon from "../../Assets/Img/darkModeIcon.svg";
-import darkModeOneTheme from "../../Assets/Img/darkModeOneTheme.svg";
-import darkModeTwoTheme from "../../Assets/Img/darkModeTwoTheme.svg";
-import logoBlue from "../../Assets/Img/logoBlue.svg";
-import logoPink from "../../Assets/Img/logoPink.svg";
 import darkModeFillTheme from "../../Assets/Img/darkModeFillTheme.svg";
 import darkModeNotActive from "../../Assets/Img/darkModeNotActive.svg";
 import pinkModeFillTheme from "../../Assets/Img/pinkModeFillTheme.svg";
@@ -28,25 +14,8 @@ import SearchBar from "../SearchBar/SearchBar";
 function Header() {
   const node = React.useRef();
   const url = useLoaderData();
-  // const [navState, setNavState] = React.useState(false);
   const [currentTheme, setCurrentTheme] = React.useState("light");
 
-  // const handleClickOutside = (e) => {
-  //   const hamButton = document.getElementById("NavBarInput");
-  //   if (node.current.contains(e.target)) {
-  //     return;
-  //   }
-
-  //   hamButton.click();
-  // };
-
-  // React.useEffect(() => {
-  //   const hamButton = document.getElementById("NavBarInput");
-
-  //   if (hamButton.checked && !navState) {
-  //     hamButton.click();
-  //   }
-  // }, []);
 
   React.useEffect(() => {
     if (localStorage.getItem("theme")) {
@@ -54,17 +23,6 @@ function Header() {
       // setCurrentTheme('light');
     }
   });
-
-  // React.useEffect(() => {
-  //   if (navState) {
-  //     document.addEventListener("mousedown", handleClickOutside);
-  //   } else {
-  //     document.removeEventListener("mousedown", handleClickOutside);
-  //   }
-  //   return () => {
-  //     document.removeEventListener("mousedown", handleClickOutside);
-  //   };
-  // }, [navState]);
 
   const dropdown = useMediaQuery({
     query: "(max-width: 1230px)",
@@ -90,350 +48,24 @@ function Header() {
 
   return (
     <nav ref={node} className="HeaderWrapper">
-      {/* {dropdown && (
-        <a href="/">
-          <img
-            className={`
-              HeaderWrapper__left--logo 
-              ${navState ? "HeaderWrapper__left--logo--blur" : ""}`}
-            src={appLogo}
-            alt="snu explore Logo"
-          />
-        </a>
-      )} */}
-
-      {/* <div
-        className={`HeaderWrapper__menu ${
-          navState ? "HeaderWrapper__menu--open" : ""
-        }`}
-      > */}
       <div className="HeaderWrapper__menu">
         <div className="HeaderWrapper__left">
-          {/* {!dropdown && ( */}
-            <>
-            <a href="/">
-              <img
-                src={appLogo}
-                alt="snu explore Logo"
-              />
-            </a>
-            <div className="HeaderWrapper__left--searchBar">
-              <SearchBar />
-            </div>
-            {/* <div className="HeaderWrapper__left--searchBar">
-              <input type="input" className="HeaderWrapper__left--searchBarInput" placeholder="Search for location, admin info, academics, food or anything"/>
-            </div> */}
-            </>
-          {/* )} */}
+          <a href="/">
+            <img src={appLogo} alt="snu explore Logo" />
+          </a>
+          <div className="HeaderWrapper__left--searchBar">
+            <SearchBar />
+          </div>
         </div>
-
-        {/* <div className="HeaderWrapper__middle">
-          {!dropdown && (
-          )}
-        </div> */}
-
-        {/* <ul className="HeaderWrapper__middle">
-          {dropdown && (
-            <div style={{ width: "100%" }}>
-              <li className="HeaderWrapper__middle--option">
-                <p>Features</p>
-              </li>
-              <div className="HeaderWrapper__middle--lineSep"></div>
-            </div>
-          )}
-          <li className="HeaderWrapper__middle--option">
-            <a href="/navigate">
-              {dropdown && (
-                <img
-                  className="HeaderWrapper__middle--option"
-                  src={locationIcon}
-                  alt="snu explore Logo"
-                />
-              )}
-              Navigate Campus
-            </a>
-          </li>
-          <li className="HeaderWrapper__middle--option">
-            <NavLink
-              to="/events"
-              className={({ isActive }) =>
-                isActive ? "activePage" : "not-active-class"
-              }
-            >
-              {dropdown && (
-                <img
-                  className="HeaderWrapper__middle--option fire"
-                  src={fireIcon}
-                  alt="snu explore Logo"
-                />
-              )}
-              Happenings@SNU
-            </NavLink>
-          </li>
-          {dropdown && (
-            <div style={{ marginTop: "17px", width: "100%" }}>
-              <li className="HeaderWrapper__middle--option">
-                <p>Help and more</p>
-              </li>
-              <div className="HeaderWrapper__middle--lineSep"></div>
-            </div>
-          )}
-          <li className="HeaderWrapper__middle--option">
-            <NavLink
-              to="/admin"
-              className={({ isActive }) =>
-                isActive ? "activePage" : "not-active-class"
-              }
-            >
-              {dropdown && (
-                <img
-                  className="HeaderWrapper__middle--option fire"
-                  src={infoIcon}
-                  alt="snu explore Logo"
-                />
-              )}
-              Admins & Contact
-            </NavLink>
-          </li>
-          <li className="HeaderWrapper__middle--option">
-            <NavLink
-              to="/faqs"
-              className={({ isActive }) =>
-                isActive ? "activePage" : "not-active-class"
-              }
-            >
-              {dropdown && (
-                <img
-                  className="HeaderWrapper__middle--option fire"
-                  src={questionIcon}
-                  alt="snu explore Logo"
-                />
-              )}
-              FAQs
-            </NavLink>
-          </li>
-          <li className="HeaderWrapper__middle--option">
-            <NavLink
-              to="/about"
-              className={({ isActive }) =>
-                isActive ? "activePage" : "not-active-class"
-              }
-            >
-              {dropdown && (
-                <img
-                  className="HeaderWrapper__middle--option fire"
-                  src={usersIcon}
-                  alt="snu explore Logo"
-                />
-              )}
-              About Us
-            </NavLink>
-          </li>
-          {dropdown && (
-            <div style={{ marginTop: "17px", width: "100%" }}>
-              <li className="HeaderWrapper__middle--option">
-                <p>Themes</p>
-              </li>
-              <div className="HeaderWrapper__middle--lineSep"></div>
-            </div>
-          )}
-          {dropdown && (
-            <li className="HeaderWrapper__middle--option">
-              <button
-                onClick={() => {
-                  document
-                    .getElementsByTagName("html")
-                    .item(0)
-                    .classList.add("light");
-                  document
-                    .getElementsByTagName("html")
-                    .item(0)
-                    .classList.remove("dark");
-                  document
-                    .getElementsByTagName("html")
-                    .item(0)
-                    .classList.remove("pink");
-                  localStorage.setItem("theme", "light");
-                }}
-              >
-                <img
-                  className="HeaderWrapper__middle--option fire"
-                  src={lightModeIcon}
-                  alt="snu explore Logo"
-                />
-                Light Mode
-              </button>
-            </li>
-          )}
-          {dropdown && (
-            <li className="HeaderWrapper__middle--option">
-              <button
-                onClick={() => {
-                  document
-                    .getElementsByTagName("html")
-                    .item(0)
-                    .classList.add("dark");
-                  document
-                    .getElementsByTagName("html")
-                    .item(0)
-                    .classList.remove("light");
-                  document
-                    .getElementsByTagName("html")
-                    .item(0)
-                    .classList.remove("pink");
-                  localStorage.setItem("theme", "dark");
-                }}
-              >
-                <img
-                  className="HeaderWrapper__middle--option"
-                  src={darkModeIcon}
-                  alt="snu explore Logo"
-                />
-                Dark Mode #1
-                <img
-                  className="HeaderWrapper__middle--option"
-                  src={darkModeOneTheme}
-                  style={{ marginLeft: "15px", filter: "none" }}
-                  alt="snu explore Logo"
-                />
-              </button>
-            </li>
-          )}
-          {dropdown && (
-            <li className="HeaderWrapper__middle--option">
-              <button
-                onClick={() => {
-                  document
-                    .getElementsByTagName("html")
-                    .item(0)
-                    .classList.remove("dark");
-                  document
-                    .getElementsByTagName("html")
-                    .item(0)
-                    .classList.remove("light");
-                  document
-                    .getElementsByTagName("html")
-                    .item(0)
-                    .classList.add("pink");
-                  localStorage.setItem("theme", "pink");
-                }}
-              >
-                <img
-                  className="HeaderWrapper__middle--option fire"
-                  src={darkModeIcon}
-                  alt="snu explore Logo"
-                />
-                Dark Mode #2
-                <img
-                  className="HeaderWrapper__middle--option fire"
-                  src={darkModeTwoTheme}
-                  style={{ marginLeft: "15px", filter: "none" }}
-                  alt="snu explore Logo"
-                />
-              </button>
-            </li>
-          )}
-        </ul> */}
 
         <div className="HeaderWrapper__right">
-          {/* {!dropdown && (
-            <div
-              className="HeaderWrapper__right--themeButtons"
-              style={{
-                background: themeStyle[currentTheme ? currentTheme : "light"],
-              }}
-            >
-            </div> */}
-            <button className="HeaderWrapper__right--contactButton">
-              Contact
-            </button>
-          {/* )} */}
-          {/* {dropdown && (
-            <div
-              className="HeaderWrapper__right--themeButtons"
-              style={{
-                background: "inherit",
-              }}
-            >
-              <p className="HeaderWrapper__right--paragraph">Social Medias</p>
-              <div className="HeaderWrapper__right--lineSep"></div>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  columnGap: "20px",
-                  paddingBottom: "50px",
-                }}
-              >
-                <a target="_blank" href="www.linkedin.com/company/snuxplore">
-                  <img
-                    className="HeaderWrapper__right--themeButtons--socialMediaIcons"
-                    src={linkedInLogo}
-                    alt="LinkedIn logo"
-                  />
-                </a>
-                <a
-                  target="_blank"
-                  href="https://www.instagram.com/snu.xplore/"
-                  rel="noreferrer"
-                >
-                  <img
-                    className="HeaderWrapper__right--themeButtons--socialMediaIcons"
-                    src={instagramLogo}
-                    alt="Instagram logo"
-                  />
-                </a>
-              </div>
-            </div>
-          )} */}
+          <button className="HeaderWrapper__right--contactButton">
+            Contact
+          </button>
         </div>
-        {/* {dropdown && (
-          <div
-            style={{ position: "relative", width: "100%", height: "1000px" }}
-          >
-            <div className="HeaderWrapper__dots">
-              <span className="HeaderWrapper__dots--dot"></span>
-              <span className="HeaderWrapper__dots--dot"></span>
-              <span className="HeaderWrapper__dots--dot"></span>
-            </div>
-          </div>
-        )} */}
       </div>
-
-      {/* <div
-        className="HeaderWrapper__Hamburger"
-        style={{
-          position: navState ? "fixed" : "",
-          top: "41px",
-          right: "30px",
-        }}
-      >
-        <input
-          type="checkbox"
-          id="NavBarInput"
-          onChange={() => {
-            setNavState(!navState);
-
-            // changed "nav ~ div" to "nav ~ *"
-            const nodeList = document.querySelectorAll("nav ~ *");
-            for (let i = 0; i < nodeList.length; i++) {
-              nodeList[i].style.filter = `${
-                !navState ? "blur(3.5px)" : "none"
-              }`;
-              nodeList[i].style.transition =
-                "0.5s filter cubic-bezier(0.77, 0.2, 0.05, 1)";
-            }
-          }}
-        />
-        <div className="hamButton">
-          <label className="HamMenu" htmlFor="NavBarInput">
-            <span className="span HL1" />
-            <span className="span HL2" />
-            <span className="span HL3" />
-          </label>
-        </div>
-      </div> */}
     </nav>
   );
 }
+
 export default Header;
